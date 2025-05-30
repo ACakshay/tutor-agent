a4cae2dd78ba7efe5931cb77a09f517f44858a04
akschord1 <akshay.chordia@publicisasapient.com>
2025-05-30 22:04:46 +0530

    added exception handling

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from google.adk.cli.fast_api import get_fast_api_app
 
-app = get_fast_api_app(agents_dir=".", web=True)
+try:
+    app = get_fast_api_app(agents_dir=".", web=True)
+except Exception as e:
+    print(repr(e))
