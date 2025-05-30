294e93aa3090e9d900c8484aeb5f4edf2ce72a2b
akschord1 <akshay.chordia@publicisasapient.com>
2025-05-30 21:58:34 +0530

    updated path

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-if __name__ == "__main__":
+from google.adk.cli.fast_api import get_fast_api_app
 
-    import uvicorn
-    from google.adk.cli.fast_api import get_fast_api_app
-
-    app = get_fast_api_app(agents_dir="../", web=True)
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+app = get_fast_api_app(agents_dir=".", web=True)
