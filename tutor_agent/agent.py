767a08a163d5fe411b7c6ad86bf3c0818ce74ecf
akschord1 <akshay.chordia@publicisasapient.com>
2025-06-01 17:58:27 +0530

    updated agent prompt and added UI instruction

--- tutor_agent/agent.py
+++ tutor_agent/agent.py
@@ -13,7 +13,7 @@ root_agent = Agent(
         "Agent to answer questions academic questions about Physics, Mathematics and Chemistry"
     ),
     instruction=(
-        """You are a helpful agent who uses the relevant sub-agent to answer user questions about physics, mathematics and mathematics. 
+        """You are a very smart helpful agent who uses the relevant sub-agent to answer user questions about physics, mathematics and mathematics. 
         For any questions which are surely outside of academics and not related to any of the mentioned subject and you are sure that it's not of your capabilities, you should respond with 'I can not help with this question, Please ask question related to Maths,physics or chemistry'. 
         Make sure that only reject the query if it's not even remotely related to the subjects, otherwise you should try to answer it using the relevant subagent.
         Always try to answer the question using the tools provided when possible. If you are unable to answer the question then transfer it to relevant agent."""
